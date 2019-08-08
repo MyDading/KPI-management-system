108ee7be4f6c5be268e936024250557c748ed337
吕运鼎 <34373078+MyDading@users.noreply.github.com>
2019-08-08 17:07:42 +0800

    Add files via upload

---
+++ WindowsFormsApp1 - 0808/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,359 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CCWin.SkinControl;
+using CCWin;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : CCSkinMain
+    {
+        
+   
+        public Form1()
+        {
+            InitializeComponent();
+            //设置标题栏无按钮
+            //this.ControlBox = false;
+            //
+
+            //多行文本框
+            cxFlatTextBox2.Text =
+                "A" + Environment.NewLine
+                + "B" + Environment.NewLine
+                + "C" + Environment.NewLine
+                + "D" + Environment.NewLine
+                + "E" + Environment.NewLine
+                + "F" + Environment.NewLine
+                + "G" + Environment.NewLine
+                + "H" + Environment.NewLine
+                + "I" + Environment.NewLine
+                + "J" + Environment.NewLine
+                + "K" + Environment.NewLine;
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //cxFlatRoundProgressBar1.ValueNumber += 10;
+            if (timer1.Enabled)
+                timer1.Stop();
+            else
+                timer1.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cxFlatContextMenuStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+            cxFlatButton2.Text = e.ClickedItem.Text;
+        }
+
+        private void cxFlatDatePicker1_onDateChanged(DateTime newDateTime)
+        {
+            label1.Text = cxFlatDatePicker1.Date.ToLongDateString();
+        }
+
+        public void Reciver(string conter) //接受登录用户名的方法，数据从Form2的tbUsername（用户名TextBox控件）中取得
+        {
+            this.userlab.Text = conter;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+            //this.Reciver("");
+            //string 登录用户 = userlab.Text.Trim();
+            // TODO: 这行代码将数据加载到表“setDataSet.kpi_info”中。您可以根据需要移动或删除它。
+            this.kpi_infoTableAdapter.Fill(this.setDataSet.kpi_info);
+            // TODO: 这行代码将数据加载到表“setDataSet.kpi”中。您可以根据需要移动或删除它。
+            this.kpiTableAdapter.Fill(this.setDataSet.kpi);
+            
+        }
+
+        private void CxFlatRoundButton1_Click(object sender, EventArgs e)
+        {
+            /*数据库连接
+            //1.创建数据连接,这里注意你登录数据库的数据库名称，用户名和密码
+            string strcon = "server=localhost;database=set;uid=root;pwd=sas124578;";
+            MySqlConnection con = new MySqlConnection(strcon);
+
+            //2. 打开数据库
+            con.Open();
+            //3. sql语句
+            string sqlSel = "server=localhost;database=set;uid=root;pwd=sas124578;";
+            MySqlCommand com = new MySqlCommand(sqlSel, con);
+            //MySqlConnection conn = new MySqlConnection(connectionstring);
+            MySqlCommand cmd = new MySqlCommand("Your sql query");
+            MySqlDataReader dr = cmd.ExecuteReader();
+
+            dataGridView1.DataSource = dr;
+            //dataGridView1.DataBindings control ;
+            */
+            string str = "server=localhost;database=set;uid=root;pwd=sas124578;";
+            MySqlConnection con = new MySqlConnection(str);//实例化链接
+            con.Open();//开启连接
+            string strcmd = "select * from kpi";
+            MySqlCommand cmd = new MySqlCommand(strcmd, con);
+            MySqlDataAdapter ada = new MySqlDataAdapter(cmd);
+            DataSet ds = new DataSet();
+            ada.Fill(ds);//查询结果填充数据集
+            dataGridView1.DataSource = ds.Tables[0];
+            con.Close();//关闭连接
+
+
+        }
+
+        private void FillToolStripButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                this.kpi_infoTableAdapter.Fill(this.setDataSet.kpi_info);
+            }
+            catch (System.Exception ex)
+            {
+                System.Windows.Forms.MessageBox.Show(ex.Message);
+            }
+
+        }
+
+        private void SkinButton11_Click(object sender, EventArgs e)
+        {
+            string str = "server=localhost;database=set;uid=root;pwd=sas124578;";
+            MySqlConnection con = new MySqlConnection(str);//实例化链接
+            con.Open();//开启连接
+            string strcmd = "select * from kpi";
+            MySqlCommand cmd = new MySqlCommand(strcmd, con);
+            MySqlDataAdapter ada = new MySqlDataAdapter(cmd);
+            DataSet ds = new DataSet();
+            ada.Fill(ds);//查询结果填充数据集
+            dataGridView1.DataSource = ds.Tables[0];
+            con.Close();//关闭连接
+        }
+
+        private void CxFlatRoundButton17_Click(object sender, EventArgs e)
+        {
+            //定义要存入数据库中的字符
+            string kpi细项 = label58.Text.Trim();
+            string 审核方式 = label57.Text.Trim();
+            string 评核计算方式 = label56.Text.Trim();
+            string 目标值 = label55.Text.Trim();
+            string 比重 = label52.Text.Trim();
+            string 项目 = cxFlatTextBox34.Text.Trim();
+            string 系统编号 = cxFlatTextBox33.Text.Trim();
+            string 细项 = cxFlatTextBox29.Text.Trim();
+            string 目标数 = cxFlatTextBox32.Text.Trim();
+            string 达成数 = cxFlatTextBox31.Text.Trim();
+            string 项次 = cxFlatTextBox30.Text.Trim();
+            string 工号 = Jobidlabel.Text.Trim();
+            string 用户名 = userlab.Text.Trim();
+            //连接mysql数据库
+            MySqlConnectionStringBuilder sb = new MySqlConnectionStringBuilder();//创建链接数据库对象SB,下面定义了连接信息
+            sb.Server = "127.0.0.1";//服务IP地址
+            sb.Port = 3306;//端口
+            sb.Database = "set";//数据库名
+            sb.UserID = "root";//用户名
+            sb.Password = "sas124578";//密码
+            // sb.CharacterSet = "gb2312";
+            MySqlConnection con = new MySqlConnection(sb.ConnectionString);//实例化连接
+            con.Open();//打开连接
+            // if (1 == 1)//管理员表
+            //  {
+            //   String query = "select * from kpi_msg where user_name='" + name + "'";//向数据库服务器发送指令
+            //   MySqlCommand cmd = new MySqlCommand(query, con);
+            //执行结果赋值到dr，dr为只读
+            //    MySqlDataReader dr = cmd.ExecuteReader();
+            //     if (dr.Read())
+            //     {
+            //         MessageBox.Show("改用户已存在，请重新注册！");
+            //    }
+            //    else
+            //     {
+
+                    string insert = "insert ignore into kpi_msg(kpi,kpi辅项,kpi细项,审核方式,评核计算方式,目标值,比重,达成数,目标数,项次,项目,系统编号,细项,工号,姓名) values " +
+                    "( '专案质量', '进度管理','" + kpi细项 + "'," + "'" + 审核方式 + "'," + "'" + 评核计算方式 + "'," + "'" + 目标值 + "'," + "'" + 比重 + "'," + "'" + 达成数 + "'," + "'" + 目标数 + "'," + "'" + 项次 + "'," + "'" + 项目 + "'," + "'" + 系统编号 + "'," + "'" + 细项 + "'," + "'" + 工号 + "'," + "'" + 用户名 + "')";
+                    MySqlCommand cmd2 = new MySqlCommand(insert, con);
+                    //dr.Close();
+                    cmd2.ExecuteNonQuery();
+                    MessageBox.Show("保存成功！");
+          //      }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void CxFlatRoundButton16_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Timer1_Tick_1(object sender, EventArgs e)
+        {
+            this.Kpi信息刷新_Click(null, null);
+            this.Kpi信息刷新1_Click(null, null);
+            timer1.Enabled = false;
+        }
+
+        private void Kpi信息刷新_Click(object sender, EventArgs e)
+        {
+            string 登录用户 = userlab.Text.Trim();
+            ///*******加载对应用户的KPI信息到页面中********///
+            MySqlConnection conn = new MySqlConnection("server=localhost;database=set;uid=root;pwd=sas124578;");
+            //conn.ConnectionString = "";
+            conn.Open();
+            MySqlCommand cmd = conn.CreateCommand();
+
+            //cmd.CommandText = "SELECT * from kpi_msg WHERE 姓名='呂運鼎'";//查找对应姓名的数据
+            //cmd.CommandText = "SELECT * from kpi_msg WHERE 姓名='" + 登录用户 + "' LIMIT 3,1";   //数据库数据建好后启用该句，查询对应登录用户信息中的第四行开始的第一条数据
+            cmd.CommandText = "SELECT * FROM kpi_msg WHERE kpi_msg.`KPI辅项` = '进度管理' AND kpi_msg.`姓名` ='" + 登录用户 + "' LIMIT 0, 1";//查询对应登录用户的进度管理的第一条信息
+            MySqlDataReader reader = cmd.ExecuteReader();
+            reader.Read();
+            try
+            {
+                string temp_role = reader.GetString(3);
+                label58.Text = temp_role;
+
+                string temp_role1 = reader.GetString(4);
+                label57.Text = temp_role1;
+
+                string temp_role2 = reader.GetString(5);
+                label56.Text = temp_role2;
+
+                string temp_role3 = reader.GetString(6);
+                label55.Text = temp_role3;
+
+                string temp_role4 = reader.GetString(7);
+                label52.Text = temp_role4;
+
+                string temp_role5 = reader.GetString(17);
+                Jobidlabel.Text = temp_role5;
+            }
+
+            catch
+            {
+                MessageBox.Show("用户数据刷新失败！");
+            }
+        /*
+            cmd.CommandText = "SELECT * FROM kpi_msg WHERE kpi_msg.`KPI辅项` = '準確度' AND kpi_msg.`姓名` ='" + 登录用户 + "' LIMIT 0, 1";
+            MySqlDataReader reader1 = cmd.ExecuteReader();
+            reader1.Read();
+            string temp_role6 = reader1.GetString(3);
+            label69.Text = temp_role6;
+            /*
+            try
+            {
+                string temp_role6 = reader.GetString(3);
+                label69.Text = temp_role6;
+            }
+            catch
+            {
+                MessageBox.Show("用户数据刷新失败！");
+            }
+            */
+            conn.Close();
+            ///*******功能结束********///
+        }
+
+        private void CxFlatRoundButton11_Click(object sender, EventArgs e)
+        {
+            //定义要存入数据库中的字符
+            string kpi细项 = label69.Text.Trim();
+            string 审核方式 = label68.Text.Trim();
+            string 评核计算方式 = label67.Text.Trim();
+            string 目标值 = label66.Text.Trim();
+            string 比重 = label63.Text.Trim();
+            string 项目 = cxFlatTextBox38.Text.Trim();
+            string 系统编号 = cxFlatTextBox37.Text.Trim();
+            string 细项 = cxFlatTextBox27.Text.Trim();
+            string 目标数 = cxFlatTextBox36.Text.Trim();
+            string 达成数 = cxFlatTextBox35.Text.Trim();
+            string 项次 = cxFlatTextBox28.Text.Trim();
+            string 工号 = Jobidlabel.Text.Trim();
+            string 用户名 = userlab.Text.Trim();
+            //连接mysql数据库
+            MySqlConnectionStringBuilder sb = new MySqlConnectionStringBuilder();//创建链接数据库对象SB,下面定义了连接信息
+            sb.Server = "127.0.0.1";//服务IP地址
+            sb.Port = 3306;//端口
+            sb.Database = "set";//数据库名
+            sb.UserID = "root";//用户名
+            sb.Password = "sas124578";//密码
+            // sb.CharacterSet = "gb2312";
+            MySqlConnection con = new MySqlConnection(sb.ConnectionString);//实例化连接
+            con.Open();//打开连接
+                       // if (1 == 1)//管理员表
+                       //  {
+                       //   String query = "select * from kpi_msg where user_name='" + name + "'";//向数据库服务器发送指令
+                       //   MySqlCommand cmd = new MySqlCommand(query, con);
+                       //执行结果赋值到dr，dr为只读
+                       //    MySqlDataReader dr = cmd.ExecuteReader();
+                       //     if (dr.Read())
+                       //     {
+                       //         MessageBox.Show("改用户已存在，请重新注册！");
+                       //    }
+                       //    else
+                       //     {
+
+            string insert = "insert ignore into kpi_msg(kpi,kpi辅项,kpi细项,审核方式,评核计算方式,目标值,比重,达成数,目标数,项次,项目,系统编号,细项,工号,姓名) values " +
+            "( '专案质量', '准确度','" + kpi细项 + "'," + "'" + 审核方式 + "'," + "'" + 评核计算方式 + "'," + "'" + 目标值 + "'," + "'" + 比重 + "'," + "'" + 达成数 + "'," + "'" + 目标数 + "'," + "'" + 项次 + "'," + "'" + 项目 + "'," + "'" + 系统编号 + "'," + "'" + 细项 + "'," + "'" + 工号 + "'," + "'" + 用户名 + "')";
+            MySqlCommand cmd2 = new MySqlCommand(insert, con);
+            //dr.Close();
+            cmd2.ExecuteNonQuery();
+            MessageBox.Show("保存成功！");
+            //      }
+        }
+
+        private void Kpi信息刷新1_Click(object sender, EventArgs e)
+        {
+            string 登录用户 = userlab.Text.Trim();
+            ///*******加载对应用户的KPI信息到页面中********///
+            MySqlConnection conn = new MySqlConnection("server=localhost;database=set;uid=root;pwd=sas124578;");
+            //conn.ConnectionString = "";
+            conn.Open();
+            MySqlCommand cmd = conn.CreateCommand();
+
+            //cmd.CommandText = "SELECT * from kpi_msg WHERE 姓名='呂運鼎'";//查找对应姓名的数据
+            //cmd.CommandText = "SELECT * from kpi_msg WHERE 姓名='" + 登录用户 + "' LIMIT 3,1";   //数据库数据建好后启用该句，查询对应登录用户信息中的第四行开始的第一条数据
+            cmd.CommandText = "SELECT * FROM kpi_msg WHERE kpi_msg.`KPI辅项` = '準確度' AND kpi_msg.`姓名` ='" + 登录用户 + "' LIMIT 0, 1";//查询对应登录用户的进度管理的第一条信息
+            MySqlDataReader reader = cmd.ExecuteReader();
+            reader.Read();
+            try
+            {
+                string temp_role = reader.GetString(3);
+                label69.Text = temp_role;
+
+                string temp_role1 = reader.GetString(4);
+                label68.Text = temp_role1;
+
+                string temp_role2 = reader.GetString(5);
+                label67.Text = temp_role2;
+
+                string temp_role3 = reader.GetString(6);
+                label66.Text = temp_role3;
+
+                string temp_role4 = reader.GetString(7);
+                label63.Text = temp_role4;
+
+            }
+
+            catch
+            {
+                MessageBox.Show("用户数据刷新失败！+1");
+            }
+            conn.Close();
+        }
+    }
+    
+}
---
+++ WindowsFormsApp1 - 0808/WindowsFormsApp1/Form2.Designer.cs
@@ -0,0 +1,284 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.btnRegister = new System.Windows.Forms.Button();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.tbPassword = new System.Windows.Forms.TextBox();
+            this.lblPassword = new System.Windows.Forms.Label();
+            this.tbUsername = new System.Windows.Forms.TextBox();
+            this.lblUsername = new System.Windows.Forms.Label();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.lblStatusResult = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(54, 288);
+            this.radioButton2.Margin = new System.Windows.Forms.Padding(4);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(141, 22);
+            this.radioButton2.TabIndex = 36;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "radioButton2";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(54, 244);
+            this.radioButton1.Margin = new System.Windows.Forms.Padding(4);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(141, 22);
+            this.radioButton1.TabIndex = 35;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "radioButton1";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(196, 189);
+            this.button5.Margin = new System.Windows.Forms.Padding(4);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(112, 34);
+            this.button5.TabIndex = 34;
+            this.button5.Text = "注册测试";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.Button5_Click_1);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(54, 188);
+            this.button4.Margin = new System.Windows.Forms.Padding(4);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(112, 34);
+            this.button4.TabIndex = 33;
+            this.button4.Text = "登录测试";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.Button4_Click_1);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(555, 72);
+            this.button3.Margin = new System.Windows.Forms.Padding(4);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(112, 34);
+            this.button3.TabIndex = 32;
+            this.button3.Text = "主界面";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.Button3_Click_1);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(616, 138);
+            this.button2.Margin = new System.Windows.Forms.Padding(4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(112, 34);
+            this.button2.TabIndex = 31;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(492, 138);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(112, 34);
+            this.button1.TabIndex = 30;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // btnRegister
+            // 
+            this.btnRegister.Location = new System.Drawing.Point(616, 190);
+            this.btnRegister.Margin = new System.Windows.Forms.Padding(4);
+            this.btnRegister.Name = "btnRegister";
+            this.btnRegister.Size = new System.Drawing.Size(112, 34);
+            this.btnRegister.TabIndex = 29;
+            this.btnRegister.Text = "注册";
+            this.btnRegister.UseVisualStyleBackColor = true;
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Location = new System.Drawing.Point(492, 190);
+            this.btnLogin.Margin = new System.Windows.Forms.Padding(4);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(112, 34);
+            this.btnLogin.TabIndex = 28;
+            this.btnLogin.Text = "登录";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            // 
+            // tbPassword
+            // 
+            this.tbPassword.Location = new System.Drawing.Point(123, 134);
+            this.tbPassword.Margin = new System.Windows.Forms.Padding(4);
+            this.tbPassword.Name = "tbPassword";
+            this.tbPassword.Size = new System.Drawing.Size(148, 28);
+            this.tbPassword.TabIndex = 27;
+            // 
+            // lblPassword
+            // 
+            this.lblPassword.AutoSize = true;
+            this.lblPassword.Location = new System.Drawing.Point(51, 138);
+            this.lblPassword.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblPassword.Name = "lblPassword";
+            this.lblPassword.Size = new System.Drawing.Size(44, 18);
+            this.lblPassword.TabIndex = 26;
+            this.lblPassword.Text = "密码";
+            // 
+            // tbUsername
+            // 
+            this.tbUsername.Location = new System.Drawing.Point(123, 68);
+            this.tbUsername.Margin = new System.Windows.Forms.Padding(4);
+            this.tbUsername.Name = "tbUsername";
+            this.tbUsername.Size = new System.Drawing.Size(148, 28);
+            this.tbUsername.TabIndex = 25;
+            // 
+            // lblUsername
+            // 
+            this.lblUsername.AutoSize = true;
+            this.lblUsername.Location = new System.Drawing.Point(51, 72);
+            this.lblUsername.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblUsername.Name = "lblUsername";
+            this.lblUsername.Size = new System.Drawing.Size(62, 18);
+            this.lblUsername.TabIndex = 24;
+            this.lblUsername.Text = "用户名";
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(616, 254);
+            this.btnClose.Margin = new System.Windows.Forms.Padding(4);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(112, 34);
+            this.btnClose.TabIndex = 23;
+            this.btnClose.Text = "断开数据库";
+            this.btnClose.UseVisualStyleBackColor = true;
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(492, 254);
+            this.btnConnect.Margin = new System.Windows.Forms.Padding(4);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(112, 34);
+            this.btnConnect.TabIndex = 22;
+            this.btnConnect.Text = "连接数据库";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            // 
+            // lblStatusResult
+            // 
+            this.lblStatusResult.AutoSize = true;
+            this.lblStatusResult.Location = new System.Drawing.Point(614, 320);
+            this.lblStatusResult.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblStatusResult.Name = "lblStatusResult";
+            this.lblStatusResult.Size = new System.Drawing.Size(134, 18);
+            this.lblStatusResult.TabIndex = 21;
+            this.lblStatusResult.Text = "数据库未连接！";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(648, 338);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 18);
+            this.label1.TabIndex = 20;
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Location = new System.Drawing.Point(489, 320);
+            this.lblStatus.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(98, 18);
+            this.lblStatus.TabIndex = 19;
+            this.lblStatus.Text = "状态指示：";
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(756, 450);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnRegister);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.tbPassword);
+            this.Controls.Add(this.lblPassword);
+            this.Controls.Add(this.tbUsername);
+            this.Controls.Add(this.lblUsername);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnConnect);
+            this.Controls.Add(this.lblStatusResult);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblStatus);
+            this.Name = "Form2";
+            this.Text = "Form2";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form2_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnRegister;
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.TextBox tbPassword;
+        private System.Windows.Forms.Label lblPassword;
+        private System.Windows.Forms.TextBox tbUsername;
+        private System.Windows.Forms.Label lblUsername;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.Label lblStatusResult;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblStatus;
+    }
+}
